9b7a6b5e33ce8a1f1e8a8ad03ae36409635dea0b
Alan Wen <alanjameswen@outlook.com>
2020-02-07 19:05:44 -0800

    Groupify Webpage
    Groupify Web now has a button that starts the party.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// code starts here //
+function onButtonClickedEvent() {
+	alert('Party Started.');
+}
+
+// button
+const element = (
+	<div>
+		<h1>Groupify Alpha</h1>
+		<button onClick={onButtonClickedEvent}>
+			Start the Party
+		</button>
+	</div>
+);
+
+// render
+ReactDOM.render(
+	element,
+	document.getElementById('root')
+);
+
+// code stops here //
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
